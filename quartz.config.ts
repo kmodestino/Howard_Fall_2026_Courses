a27a6e21ee630bc4bdf45cb5248a8fd7dfedae4e
Kevin <kevin.modestino@gmail.com>
2026-01-05 12:38:22 -0500

    manual refresh push

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "kmodestino.github.io/Howard_Spring2026_Courses",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
